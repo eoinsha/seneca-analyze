0fb40c6017836ab86cbbb3586c2b7974e123f6c2
Eoin Shanaghy <eoin.shanaghy@gmail.com>
2015-08-25 09:48:10 +0100

    Finding actions in seneca files

--- main.js
+++ main.js
@@ -9,14 +9,27 @@ var jsFile = argv._[0]
 var source = fs.readFileSync(jsFile, 'UTF-8')
 var ast = esprima.parse(source)
 
+var stringifyExpression = function(expr) {
+    switch(expr.type) {
+        case "Literal": return expr.raw
+        case "Identifier": return expr.name
+        case "MemberExpression" : return expr.object.name + '.' + expr.property.name
+            return null
+    }
+}
+
 walk(ast, function(node) {
     if(node.type === "CallExpression" && _.get(node, 'callee.object.name') === "seneca") {
-        console.log(node)
+        var argProperties = _.chain(node.arguments).pluck('properties').flatten().value()
+        var callArgSummary = _.reduce(argProperties, function(acc, property) {
+            if(_.isUndefined(property)) return acc
+            var key = stringifyExpression(property.key)
+            var value = stringifyExpression(property.value)
+            if(key != null && value != null && _.includes(['role', 'cmd'], key)) {
+                return acc + (acc === "" ? "" : ',' ) + key + '=' + value
+            }
+            else return acc
+        }, '')
+        console.log(callArgSummary)
     }
 })
-
-
-
-
-
-
