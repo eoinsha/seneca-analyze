079693310427d8c1ea4b6f7002998dda63bae302
Eoin Shanaghy <eoin.shanaghy@gmail.com>
2015-08-25 11:40:25 +0100

    Text output of act/add calls in a project's sources

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-Static analzyer for Seneca projects.
-Used to reveal dependencies between services.
+# Static analzyer for Seneca projects.
+Used to reveal dependencies between microservices in projects using [senecajs.org](http://senecajs.org).
+
+## Usage
+
+```shell
+npm install
+
+node main.js /path/to/project/root
+```
--- main.js
+++ main.js
@@ -1,35 +1,18 @@
-var fs = require('fs')
-
-var esprima = require('esprima')
-var walk = require( 'esprima-walk' )
 var argv = require('yargs').argv
 var _ = require('lodash')
+var globby = require('globby')
 
-var jsFile = argv._[0]
-var source = fs.readFileSync(jsFile, 'UTF-8')
-var ast = esprima.parse(source)
+var findSenecaUsages = require('./find_seneca_usages')
 
-var stringifyExpression = function(expr) {
-    switch(expr.type) {
-        case "Literal": return expr.raw
-        case "Identifier": return expr.name
-        case "MemberExpression" : return expr.object.name + '.' + expr.property.name
-            return null
-    }
-}
+var projectRoot = process.cwd()
+if(argv._.length > 0) projectRoot = argv._[0]
 
-walk(ast, function(node) {
-    if(node.type === "CallExpression" && _.get(node, 'callee.object.name') === "seneca") {
-        var argProperties = _.chain(node.arguments).pluck('properties').flatten().value()
-        var callArgSummary = _.reduce(argProperties, function(acc, property) {
-            if(_.isUndefined(property)) return acc
-            var key = stringifyExpression(property.key)
-            var value = stringifyExpression(property.value)
-            if(key != null && value != null && _.includes(['role', 'cmd'], key)) {
-                return acc + (acc === "" ? "" : ',' ) + key + '=' + value
-            }
-            else return acc
-        }, '')
-        console.log(callArgSummary)
-    }
-})
+
+globby(['**/*.js', '!node_modules/**', '!e2e-tests/**', '!tests/**', '!test/**', '!Gruntfile.js', '!Gulpfile.js', '!grunt/**', '!web/**', '!scripts/**'],
+    { cwd: projectRoot },
+    function(err, files) {
+        console.log('PATHS', JSON.stringify(files))
+        _.forEach(files, function(file) {
+            findSenecaUsages(projectRoot, file)
+        })
+    })
