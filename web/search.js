832bb3887f5e4dae8d3ea9b1732e546f529fefd7
Eoin Shanaghy <eoin.shanaghy@gmail.com>
2015-09-04 19:40:38 +0100

    Search with Ctrl-F

--- web/search.js
+++ web/search.js
@@ -1,3 +1,42 @@
-/**
- * Created by eoin on 03/09/15.
- */
+var addSearchSupport = function() {
+    $(window).keydown(function (e) {
+        if ((e.ctrlKey || e.metaKey) && e.keyCode === 70) {
+            toggleSearch();
+            e.preventDefault()
+        }
+    });
+
+}
+
+var hideSearch = function() {
+    $('#search').hide()
+}
+
+var showSearch = function() {
+    $('#search').show()
+    $('#searchBox').focus().select()
+}
+
+var toggleSearch = function() {
+    if($('#search').is(':visible')) {
+        hideSearch()
+    }
+    else {
+        showSearch();
+    }
+}
+
+$(document).ready(function() {
+    addSearchSupport()
+
+    $('#searchBox').on('input', function() {
+        applyFilter($(this).val())
+    })
+
+    $('#searchBox').keyup(function(e) {
+        if(e.keyCode == 27) {
+            $(this).val('').change()
+            hideSearch()
+        }
+    })
+})
--- web/senecaGraph.js
+++ web/senecaGraph.js
@@ -76,8 +76,8 @@ var renderData = function(data) {
                 min: 10,
                 max: 30,
                 label: {
-                    min: 8,
-                    max: 30,
+                    min: 12,
+                    max: 40,
                     drawThreshold: 12
 //                            maxVisible: 20
                 }
@@ -100,13 +100,15 @@ var renderData = function(data) {
             }
         }
     }
-    nodesDataset = new vis.DataSet(nodes);
-    edgesDataset = new vis.DataSet(edges);
+    nodesDataset = new vis.DataSet(nodes)
+    edgesDataset = new vis.DataSet(edges)
 
     network = new vis.Network(container, {nodes: nodesDataset, edges: edgesDataset}, options)
 
-    allNodes = nodesDataset.get({returnType:"Object"});
-    network.on("click", highlightNeighbours)
+    allNodes = nodesDataset.get({returnType:"Object"})
+    network.on("click", function(params) {
+        highlightNeighbours(params.nodes)
+    })
     network.on("doubleClick", function(params) {
         if (params.nodes.length == 1) {
             if (network.isCluster(params.nodes[0]) == true) {
@@ -119,38 +121,70 @@ var renderData = function(data) {
     })
 }
 
-var highlightNeighbours = function(params) {
-    if (params.nodes.length > 0) {
-        highlightActive = true;
-        var i,j;
-        var selectedNode = params.nodes[0];
-        var degrees = 2;
+var applyFilter = function(filterString) {
+    var excludedNodes = filterString.length === 0 ? [] : _.pick(allNodes, function(node) {
+        var label = node.label || node.hiddenLabel
+        return !label || label.indexOf(filterString) === -1
+    })
+
+    _.forEach(allNodes, function(node) {
+        console.log('ALL', node.hiddenLabel, node.label)
+        node.excluded = false
+    })
+    _.forEach(excludedNodes, function(node) {
+        console.log('EXCLUDED', node.hiddenLabel, node.label)
+        node.color = 'rgba(200,200,200,0.2)'
+        node.excluded = true
+        if(node.label !== undefined) {
+            node.hiddenLabel = node.label
+            node.label = undefined
+        }
+    })
+    _.forEach(allNodes, function(node) {
+        if(!node.excluded) {
+            console.log('PRESENT', node.hiddenLabel, node.label)
+            node.color = undefined // Restore default
+            if(node.hiddenLabel !== undefined) {
+                node.label = node.hiddenLabel
+                node.hiddenLabel = undefined
+            }
+        }
+    })
+    updateNodes()
+}
+
+var highlightNeighbours = function(nodes) {
+    if (nodes.length > 0) {
+        highlightActive = true
+        var i,j
+        var selectedNode = nodes[0]
+        var degrees = 2
 
         // Grey out distant nodes
         for (var nodeId in allNodes) {
-            allNodes[nodeId].color = 'rgba(200,200,200,0.5)';
+            allNodes[nodeId].color = 'rgba(200,200,200,0.5)'
             if (allNodes[nodeId].hiddenLabel === undefined) {
-                allNodes[nodeId].hiddenLabel = allNodes[nodeId].label;
-                allNodes[nodeId].label = undefined;
+                allNodes[nodeId].hiddenLabel = allNodes[nodeId].label
+                allNodes[nodeId].label = undefined
             }
         }
-        var connectedNodes = network.getConnectedNodes(selectedNode);
-        var allConnectedNodes = [];
+        var connectedNodes = network.getConnectedNodes(selectedNode)
+        var allConnectedNodes = []
 
         // get the second degree nodes
         for (i = 1; i < degrees; i++) {
             for (j = 0; j < connectedNodes.length; j++) {
-                allConnectedNodes = allConnectedNodes.concat(network.getConnectedNodes(connectedNodes[j]));
+                allConnectedNodes = allConnectedNodes.concat(network.getConnectedNodes(connectedNodes[j]))
             }
         }
 
         // all second degree nodes get a different color and their label back
         for (i = 0; i < allConnectedNodes.length; i++) {
             if(allNodes[allConnectedNodes[i]]) {
-                allNodes[allConnectedNodes[i]].color = 'rgba(150,150,150,0.75)';
+                allNodes[allConnectedNodes[i]].color = 'rgba(150,150,150,0.75)'
                 if (allNodes[allConnectedNodes[i]].hiddenLabel !== undefined) {
-                    allNodes[allConnectedNodes[i]].label = allNodes[allConnectedNodes[i]].hiddenLabel;
-                    allNodes[allConnectedNodes[i]].hiddenLabel = undefined;
+                    allNodes[allConnectedNodes[i]].label = allNodes[allConnectedNodes[i]].hiddenLabel
+                    allNodes[allConnectedNodes[i]].hiddenLabel = undefined
                 }
             }
         }
@@ -158,43 +192,47 @@ var highlightNeighbours = function(params) {
         // all first degree nodes get their own color and their label back
         for (i = 0; i < connectedNodes.length; i++) {
             if(allNodes[connectedNodes[i]]) {
-                allNodes[connectedNodes[i]].color = undefined;
+                allNodes[connectedNodes[i]].color = undefined
                 if (allNodes[connectedNodes[i]].hiddenLabel !== undefined) {
-                    allNodes[connectedNodes[i]].label = allNodes[connectedNodes[i]].hiddenLabel;
-                    allNodes[connectedNodes[i]].hiddenLabel = undefined;
+                    allNodes[connectedNodes[i]].label = allNodes[connectedNodes[i]].hiddenLabel
+                    allNodes[connectedNodes[i]].hiddenLabel = undefined
                 }
             }
         }
 
         // the main node gets its own color and its label back.
         if(allNodes[selectedNode]) {
-            allNodes[selectedNode].color = undefined;
+            allNodes[selectedNode].color = undefined
             if (allNodes[selectedNode].hiddenLabel !== undefined) {
-                allNodes[selectedNode].label = allNodes[selectedNode].hiddenLabel;
-                allNodes[selectedNode].hiddenLabel = undefined;
+                allNodes[selectedNode].label = allNodes[selectedNode].hiddenLabel
+                allNodes[selectedNode].hiddenLabel = undefined
             }
         }
     }
     else if (highlightActive === true) {
         // reset all nodes
         for (var nodeId in allNodes) {
-            allNodes[nodeId].color = undefined;
+            allNodes[nodeId].color = undefined
             if (allNodes[nodeId].hiddenLabel !== undefined) {
-                allNodes[nodeId].label = allNodes[nodeId].hiddenLabel;
-                allNodes[nodeId].hiddenLabel = undefined;
+                allNodes[nodeId].label = allNodes[nodeId].hiddenLabel
+                allNodes[nodeId].hiddenLabel = undefined
             }
         }
         highlightActive = false
     }
 
+    updateNodes()
+}
+
+var updateNodes = function() {
     // transform the object into an array
-    var updateArray = [];
+    var updateArray = []
     for (nodeId in allNodes) {
         if (allNodes.hasOwnProperty(nodeId)) {
-            updateArray.push(allNodes[nodeId]);
+            updateArray.push(allNodes[nodeId])
         }
     }
-    nodesDataset.update(updateArray);
+    nodesDataset.update(updateArray)
 }
 
 var clusterBySource = function(sourceId) {
@@ -205,17 +243,17 @@ var clusterBySource = function(sourceId) {
             return childOptions.group === sourceId; // the color is fully defined in the node.
         },
         processProperties: function (clusterOptions, childNodes, childEdges) {
-            var totalMass = 0;
+            var totalMass = 0
             for (var i = 0; i < childNodes.length; i++) {
-                totalMass += childNodes[i].mass;
+                totalMass += childNodes[i].mass
             }
-            clusterOptions.mass = totalMass;
-            return clusterOptions;
+            clusterOptions.mass = totalMass
+            return clusterOptions
         },
         clusterNodeProperties: {id: clusterId, borderWidth: 3, shape: 'square', label:sourceNode.options.label,
             color: sourceNode.options.color, font: {color: sourceNode.options.font.color}}
     }
-    network.cluster(clusterOptionsByData);
+    network.cluster(clusterOptionsByData)
     allNodes[clusterId] = clusterOptionsByData.clusterNodeProperties
 }
 
